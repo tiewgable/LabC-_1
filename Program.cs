bf5ad9c6dc63eee8dcf528659d0eca989c9a7aca
Acer Aspire 3 <Acer Aspire 3@DESKTOP-3O0TCLQ>
2023-05-02 23:10:09 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Name : ");
+string name = Console.ReadLine();
+Console.Write("Input Weight : ");
+double weight = double.Parse(Console.ReadLine());
+Console.Write("Input Height : ");
+double height = double.Parse(Console.ReadLine());
+double BMI1 = Math.Round(weight / Math.Pow(height / 100, 2), 2);
+Console.WriteLine("name : {name} BMI = {BMI1}");
